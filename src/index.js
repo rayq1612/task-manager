1d68cdd81d4df9706776adf567f70da161ec610d
Rameshk Amir <rameshkamir@gmail.com>
2024-08-26 20:42:08 +0300

    Async refactoring

--- src/index.js
+++ src/index.js
@@ -2,80 +2,100 @@ const express = require('express')
 const {ObjectId} = require("mongodb");
 const User = require('./models/User')
 const Task = require('./models/Task')
-
-//Connect to mongoDb
 require('./db/mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
-
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
-  user.save().then(() => {
+  try {
+    await user.save()
     res.status(201).send(user)
-  }).catch((e) => {
+  } catch (e) {
     res.status(500).send({error: e})
-  })
+  }
 })
 
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
-    res.send(users)
-  }).catch((e) => {
-    res.status(400).send({error: e})
-  })
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find()
+    if (users.length === 0) {
+      res.status(404).send({error: 'Users not found'})
+    } else {
+      res.status(200).send(users)
+    }
+  } catch (e) {
+    res.status(500).send({error: e})
+  }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+    try {
+      const user = await User.findById(_id)
       if (user) {
-        return res.status(200).send(user)
+        res.status(200).send(user)
       } else {
-        return res.status(404).send({error: 'User not found'})
+        res.status(404).send({error: 'User not found'})
       }
-    }).catch((e) => {
-      return res.status(500).send({
-        description: "You must provide valid id of the user",
-        error: e
-      })
-    })
+    } catch (e) {
+        res.status(500).send({error: e})
+    }
 })
 
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body)
-  task.save().then(() => {
-    res.status(201).send(task)
-  }).catch((e) => {
-    res.status(400).send({error: e})
-  })
-})
-
-app.get('/tasks', (req, res) => {
-  Task.find({}).then((tasks) => {
-    res.send(tasks)
-  }).catch((e) => {
-    res.status(500).send({error: e})
-  })
-})
-
-app.get('/tasks/:id', (req, res) => {
+app.patch('/users/:id', async (req, res) => {
   const _id = req.params.id
-  Task.findById(_id).then((task) => {
-    if (task) {
-      return res.status(200).send(task)
-    }
-    else {
-      return res.status(404).send({error: 'Task not found'})
-    }
-  }).catch((e) => {
-    return res.status(400).send({
-      description: "You must provide valid id of the task",
-      error: e
+  try {
+    const user = await User.findByIdAndUpdate(_id, {
+      name: 'Dimon'
     })
-  })
+    if (user) {
+      res.status(200).send(user)
+    } else {
+      res.status(404).send({error: 'User not found'})
+    }
+  } catch (e) {
+    res.status(500).send({error: e})
+  }
+})
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body)
+  try {
+    const created_task = await task.save()
+    res.status(201).send(created_task)
+  } catch (e) {
+    res.status(400).send({error: e})
+  }
+})
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find()
+    if (tasks.length === 0) {
+      res.status(404).send({error: 'No tasks found'})
+    } else {
+      res.status(200).send(tasks)
+    }
+  } catch (e) {
+    res.status(500).send({error: e})
+  }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+  try {
+    const task = await Task.findById(_id)
+    if (task) {
+      res.status(200).send(task)
+    } else {
+      res.status(404).send({error: 'No task found'})
+    }
+  } catch (e) {
+    res.status(500).send({error: e})
+  }
 })
 
 app.listen(port, () => {
